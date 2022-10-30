98d54bc74d78f04ff2f8004c777e7be560ef0efe
Maxwell Knowles <maxknowles27@gmail.com>
2022-10-30 17:57:21 -0600

    Update startups.py

--- startups.py
+++ startups.py
@@ -3,9 +3,9 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
 import pandas as pd
