dccb6009964bcdfb719a0b5e8d2095770b329aa1
Maxwell Knowles <maxknowles27@gmail.com>
2022-10-31 00:03:52 -0600

    Update startups.py

--- startups.py
+++ startups.py
@@ -3,6 +3,7 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 #from selenium.webdriver.support import expected_conditions as EC
 #from selenium.webdriver.support.ui import WebDriverWait
 #from selenium.webdriver.common.action_chains import ActionChains
